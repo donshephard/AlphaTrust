f640ba0ccee51e4150ea8dc89151aaf62e58b8a1
Don Shephard <dshephard@ipipeline.com>
2021-03-27 20:36:50 +0000

    add powershell script to update project info during build

---
+++ AddAssemblyInfo.ps1
@@ -0,0 +1,25 @@
+param($path, $version)
+
+$projectPath = Resolve-Path -Path $path;
+Write-Host "Updating $projectPath";
+
+$project = [xml](Get-Content $projectPath);
+
+$assemblyVersion = $project.CreateElement('AssemblyVersion');
+$assemblyVersion.InnerXml = $version;
+
+$assemblyCompany = $project.CreateElement('AssemblyCompany');
+$assemblyCompany.InnerXml = 'AlphaTrust Corporation';
+
+;
+$assemblyCopyright = $project.CreateElement('AssemblyCopyright');
+$assemblyCopyright.InnerXml = [char]0x00A9 + "$year AlphaTrust Corporation";
+
+$propertyGroup = $project.CreateElement('PropertyGroup');
+$propertyGroup.AppendChild($assemblyVersion);
+$propertyGroup.AppendChild($assemblyCompany);
+$propertyGroup.AppendChild($assemblyCopyright);
+
+$project.Project.AppendChild($propertyGroup);
+
+$project.Save($projectPath);
