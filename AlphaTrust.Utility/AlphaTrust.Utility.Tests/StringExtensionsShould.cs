8776a1835440afcf79241268a327ec5afdcff2f5
Don Shephard <dshephard@ipipeline.com>
2021-03-27 15:52:19 +0000

    add AlphaTrust.Utility

---
+++ AlphaTrust.Utility/AlphaTrust.Utility.Tests/StringExtensionsShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AlphaTrust.Utility.Tests
+{
+    public class StringExtensionsShould
+    {
+        [Theory]
+        [InlineData("", true, true)]
+        [InlineData(" ", false, false)]
+        [InlineData(" ", true, true)]
+        public void DetermineIfStringIsEmpty(string value, bool trimBefore, bool expected)
+        {
+            // Arrange
+
+            // Act
+            var sut = value.IsEmpty(trimBefore);
+
+            // Assert
+            sut.Should().Be(expected);
+        }
+    }
+}
---
+++ AlphaTrust.Utility/AlphaTrust.Utility/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AlphaTrust.Utility
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(this string value, bool trimBefore = true)
+        {
+            return string.IsNullOrEmpty(value) && !trimBefore || trimBefore && string.IsNullOrEmpty(value?.Trim());
+        }
+    }
+}
